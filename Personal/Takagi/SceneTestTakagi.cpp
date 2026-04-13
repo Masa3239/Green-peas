1ce5275b520be34b9656653814d4e1b01da7ff9d
kana-bunbun <gaomuke186@gmail.com>
2026-04-13 17:26:22 +0900

    HP関連の準備

--- Personal/Takagi/SceneTestTakagi.cpp
+++ Personal/Takagi/SceneTestTakagi.cpp
@@ -1,10 +1,12 @@
 #include "SceneTestTakagi.h"
 #include <DxLib.h>
 #include"../../Personal/Takagi/Player.h"
+#include"../../Personal/Asai/Camera.h"
 
 SceneTestTakagi::SceneTestTakagi()
 {
 	m_pPlayer = std::make_unique<Player>();
+	m_pCamera = std::make_unique<Camera>();
 }
 
 SceneTestTakagi::~SceneTestTakagi()
@@ -14,22 +16,32 @@ SceneTestTakagi::~SceneTestTakagi()
 void SceneTestTakagi::Init()
 {
 	m_pPlayer->Init();
+	m_pCamera->Init();
+	m_pPlayer->SetCamera(m_pCamera.get());
 }
 
 void SceneTestTakagi::End()
 {
 	m_pPlayer->End();
+	m_pCamera->End();
 
 }
 
 SceneBase* SceneTestTakagi::Update()
 {
 	m_pPlayer->Update();
+	//m_pPlayer->Update();
 	return this;
 }
 
 void SceneTestTakagi::Draw()
 {
+	SetDrawScreen(m_pCamera->GetWorldScreen());
+	ClearDrawScreen();
 	printfDx("SceneTestTakagi\n");
 	m_pPlayer->Draw();
+
+	SetDrawScreen(DX_SCREEN_BACK);
+	m_pCamera->Draw();
+	m_pCamera->DebugDraw();
 }
