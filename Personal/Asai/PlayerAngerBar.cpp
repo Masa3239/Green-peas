52eb20cd121ae444e401a13cb085e11ae8eaf4b1
sakana619 <a.rihito619@icloud.com>
2026-04-15 14:42:02 +0900

    UI追加

---
+++ Personal/Asai/PlayerAngerBar.cpp
@@ -0,0 +1,83 @@
+#include "PlayerAngerBar.h"
+
+
+#include<DxLib.h>
+#include"../../Utility/Game.h"
+
+#include"../Takagi/Player.h"
+
+namespace {
+
+    const char* const kGraphHandlePath = "";
+
+    constexpr float kGaugeLeft = Game::kScreenWidth - 50;
+
+    constexpr float kGaugeRight = Game::kScreenWidth - 20;
+
+    constexpr float kGaugeTop = Game::kScreenHeight / 2;
+
+    constexpr float kGaugeBottom = Game::kScreenHeight - 70;
+
+}
+
+PlayerAngerBar::PlayerAngerBar()
+{
+}
+
+void PlayerAngerBar::Init()
+{
+    //画像の読み込み予定
+    //m_graphHandle = LoadGraph(kGraphHandlePath);
+
+}
+
+void PlayerAngerBar::Update()
+{
+}
+
+void PlayerAngerBar::Draw()
+{
+
+    //描画しない設定なら描画しない
+    if (m_isVisible)return;
+
+    //仮
+    DrawBox(kGaugeLeft, kGaugeTop, kGaugeRight, kGaugeBottom, 0xffffff, true);
+
+    float angerGaugeHeight = (kGaugeBottom - 3) - (kGaugeTop + 3);
+
+    DrawBox(kGaugeLeft,
+        kGaugeBottom - 3 - (angerGaugeHeight * m_rate),
+        kGaugeRight,
+        kGaugeBottom - 3,
+        0xff0000, TRUE
+    );
+
+}
+
+void PlayerAngerBar::DebugDraw()
+{
+
+    printfDx("UI playerAnger %f\n", m_value);
+    printfDx("UI playerAngerMax %f\n", m_max);
+    printfDx("UI playerAngerRate %f\n", m_rate);
+
+}
+
+void PlayerAngerBar::End()
+{
+}
+
+PlayerUI::DrawType PlayerAngerBar::GetDrawType() const
+{
+    return PlayerUI::DrawType::Screen;
+}
+
+void PlayerAngerBar::SetPlayer(Player* pPlayer)
+{
+
+    m_value = pPlayer->GetGaugeCurrentValue(Player::Anger);
+    m_max = pPlayer->GetGaugeMaxValue(Player::Anger);
+    m_rate = pPlayer->GetGaugeRate(Player::Anger);
+
+}
