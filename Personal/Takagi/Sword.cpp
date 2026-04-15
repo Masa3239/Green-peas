6491b4fe37821d52018b62a6eac86bfcf66a5c34
kana-bunbun <gaomuke186@gmail.com>
2026-04-15 17:36:29 +0900

    武器：剣の仮実装

--- Personal/Takagi/Sword.cpp
+++ Personal/Takagi/Sword.cpp
@@ -3,24 +3,29 @@
 #include"../../Utility/MyMath.h"
 #include"../../Utility/Transform.h"
 #include"../../Utility/Vector3.h"
+#include"../../Utility/Time.h"
 #include<DxLib.h>
+#include<math.h>
 namespace {
 	const char* const kFilePath = "Personal\\Takagi\\Resource\\Golden Sword.png";
 	/// <summary>
 	/// 武器の表示座標
 	/// </summary>
-	constexpr Vector3 kOffset = { 30.0f,0.0f,0.0f };
-	constexpr float kLerpSwing = 2.0f;
+	constexpr Vector3 kOffset = { -13.0f,10.0f,0.0f };
+	constexpr float kLerpSwing = 12.0f;
 	constexpr Transform kSwingUp = {
 		{0,0,0},
 		{0,60 * MyMath::ToRadian,0},
 		{0,0,0}
 	};
 	constexpr float kShowRadian = -45 * MyMath::ToRadian;
+	constexpr float kSwingRadian = 60 * MyMath::ToRadian;
+	constexpr float kColRadius = 10;
 }
 
-Sword::Sword(ObjectManager* objManager):
-	GameObject(objManager)
+Sword::Sword(ObjectManager* objManager) :
+	Weapon(objManager),
+	attack(false)
 {
 	// グラフィックハンドルの読み込み
 	m_graphHandle = LoadGraph(kFilePath);
@@ -30,7 +35,9 @@ Sword::Sword(ObjectManager* objManager):
 	
 	// 武器を振る際のTransformを初期化
 	m_swing.Reset();
-
+	m_desireRadian = 0;
+	m_swingState = Swing::Normal;
+	m_circle = Collision::Circle(Vector3(), kColRadius);
 }
 
 Sword::~Sword()
@@ -48,28 +55,86 @@ void Sword::End()
 
 void Sword::Update()
 {
-	m_swing.rotation.y += 2*MyMath::ToRadian;
+	float time = Time::GetInstance().GetDeltaTime();
+	float differ = m_desireRadian - m_swing.rotation.y;
+	differ = MyMath::NormalizeAngle(differ * MyMath::ToDegree) * MyMath::ToRadian;
+	m_swing.rotation.y += differ*kLerpSwing * time;
+	if (differ * differ < 0.1f * 0.1f) {
+		if (m_swingState == Swing::Up) {
+			m_desireRadian = m_attackRadian - kSwingRadian;
+			m_swingState = Swing::Down;
+		}
+		else if (m_swingState == Swing::Down) {
+			m_desireRadian = m_attackRadian;
+			attack = false;
+			m_swingState = Swing::Normal;
+		}
+	}
+
+	Vector3 drawPos = GetTransform().position;
+	if (GetTransform().rotation.y >= 0) {
+		drawPos.x += kOffset.x;
+		drawPos.y += kOffset.y;
+	}
+	else {
+		drawPos.x -= kOffset.x;
+		drawPos.y += kOffset.y;
+	}
+
+	if (attack) {
+	drawPos.x = sinf(m_swing.rotation.y);
+	drawPos.y = -cosf(m_swing.rotation.y);
+	drawPos = drawPos.GetNormalize();
+	//if (attack) {
+		drawPos *= 60;
+	/*}
+	else
+	{
+		drawPos *= 30;
+	}*/
+	drawPos += GetTransform().position;
+	}
+	else {
+		if (GetTransform().rotation.y >= 0) {
+		m_desireRadian = -150*MyMath::ToRadian;
+		}
+		else {
+		m_desireRadian = 150*MyMath::ToRadian;
+		}
+		m_attackRadian= GetTransform().rotation.y;
+	}
+	m_swing.position += (drawPos - m_swing.position) * 50 * time;
+	m_circle.SetPosition(drawPos);
 }
 
 void Sword::Draw()
 {
 	bool reverseX = false;
-	Vector3 drawPos = GetTransform().position;
-	if (GetTransform().rotation.y>=0) {
-		drawPos += kOffset;
+	if (GetTransform().rotation.y >= 0) {
 		reverseX = false;
 	}
 	else {
-		drawPos -= kOffset;
 		reverseX = true;
 	}
-	float radian = GetTransform().rotation.y+m_swing.rotation.y + (kShowRadian)*MyMath::Sign(GetTransform().rotation.y);
-	DrawRotaGraph(drawPos.x, drawPos.y, 1, radian, m_graphHandle, TRUE,reverseX);
+	float radian = /*GetTransform().rotation.y+*/m_swing.rotation.y + (kShowRadian)*MyMath::Sign(GetTransform().rotation.y);
+
+	//if (attack) {
+	//}
+	//else {
+	//	radian = 0;
+	//}
+
+	DrawRotaGraph(m_swing.position.x, m_swing.position.y, 1, radian, m_graphHandle, TRUE, reverseX);
 	printfDx("角度 : %f\n", GetTransform().rotation.y);
 	printfDx("attack : %d\n", attack);
+	m_circle.DebugDraw();
 }
 
 void Sword::Attack()
 {
-	attack = !attack;
+	if (m_swingState != Swing::Normal)return;
+	m_desireRadian = m_attackRadian + kSwingRadian;
+	m_swing.rotation.y = m_desireRadian;
+	attack = true;
+	m_swingState = Swing::Up;
 }
