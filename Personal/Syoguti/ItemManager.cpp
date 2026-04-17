3028e934bde1e3bd4974e7cf146e3118f69b83e5
Masa3239 <kobura3239@gmail.com>
2026-04-17 11:25:07 +0900

    画像ファイルの移動&画像パスの変更

--- Personal/Syoguti/ItemManager.cpp
+++ Personal/Syoguti/ItemManager.cpp
@@ -7,8 +7,8 @@
 namespace {
 
 	// 画像のファイルパス
-	const char* const kHpHealItemGraphHandlePath = "Personal\\Syoguti\\Resource\\ItemTest1.png";
-	const char* const kAttackUpItemGraphHandlePath = ".\\Personal\\Syoguti\\Resource\\ItemTest2.png";
+	const char* const kHpHealItemGraphHandlePath = ".\\Resource\\ItemTest1.png";
+	const char* const kAttackUpItemGraphHandlePath = ".\\Resource\\ItemTest2.png";
 }
 
 ItemManager::ItemManager() :
