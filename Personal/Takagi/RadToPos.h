e8f3f5c32ec8b1c1aa54e164e2a5ce403a6c32e3
kana-bunbun <gaomuke186@gmail.com>
2026-04-19 00:00:17 +0900

    ラジアン角から座標を計算する関数

---
+++ Personal/Takagi/RadToPos.h
@@ -0,0 +1,14 @@
+#pragma once
+#include<math.h>
+#include"../../Utility/Vector3.h"
+namespace {
+	Vector3 RadToPos(float radian, float distance, const Vector3& basePos = { 0.0f,0.0f,0.0f }) {
+		Vector3 res = { 0.0f,0.0f,0.0f };
+		res.x = sinf(radian);
+		res.y = -cosf(radian);
+		res = res.GetNormalize();
+		res *= distance;
+		res += basePos;
+		return res;
+	}
+}
