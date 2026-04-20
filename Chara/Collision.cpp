6e849030f7d38c865cfd1fd76cef7e32c4173af4
Lukia56 <kky56saqqe@gmail.com>
2026-04-20 15:24:45 +0900

    デッドゾーンモディファイア追加

--- Chara/Collision.cpp
+++ Chara/Collision.cpp
@@ -310,8 +310,6 @@ namespace Collision
 		DrawLine(vertices[1].x, vertices[1].y, vertices[3].x, vertices[3].y, Color::kWhite);
 		DrawLine(vertices[3].x, vertices[3].y, vertices[2].x, vertices[2].y, Color::kWhite);
 		DrawLine(vertices[2].x, vertices[2].y, vertices[0].x, vertices[0].y, Color::kWhite);
-
-		DrawBox(m_centerPos.x + dirLeftTop.x, m_centerPos.y + dirLeftTop.y, m_centerPos.x + dirRightBottom.x, m_centerPos.y + dirRightBottom.y, Color::kGray, 0);
 	}
 
 	void OBB::UpdateDirVector()
--- System/InputManager.cpp
+++ System/InputManager.cpp
@@ -11,6 +11,7 @@
 #include "../System/Input/Device/InputDeviceGamepad.h"
 #include "../System/Input/Literal/InputProperty.h"
 #include "../System/Input/Literal/InputActions.h"
+#include "../System/Input/Modifier/InputModifierDeadzone.h"
 #include "../System/Input/Modifier/InputModifierNegate.h"
 #include "../System/Input/Modifier/InputModifierSwizzleAxis.h"
 #include "../Utility/MyMath.h"
@@ -50,7 +51,11 @@ bool InputManager::Initialize()
 	Bind(Input::Action::Left, Input::Device::Gamepad, KeyCode::Button::GpFaceLeft);
 	Bind(Input::Action::Right, Input::Device::Gamepad, KeyCode::Button::GpFaceRight);
 
-	Bind(Input::Action::Move, Input::Device::Gamepad, KeyCode::Button::GpLeftThumb, {std::make_shared<InputModifierNegate>(false, true)});
+	Bind(Input::Action::Move, Input::Device::Gamepad, KeyCode::Button::GpLeftThumb,
+		{
+			std::make_shared<InputModifierNegate>(false, true),
+			std::make_shared<InputModifierDeadzone>()
+		});
 	Bind(Input::Action::Move, Input::Device::Keyboard, KeyCode::Button::W,
 		{
 			std::make_shared<InputModifierNegate>(true, true),
