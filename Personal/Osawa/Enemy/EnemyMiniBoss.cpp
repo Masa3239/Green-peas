f6caf6163e713617581134598995b27ac0791d50
Lukia56 <kky56saqqe@gmail.com>
2026-04-21 10:31:00 +0900

    中ボス

---
+++ Personal/Osawa/Enemy/EnemyMiniBoss.cpp
@@ -0,0 +1,30 @@
+#include "EnemyMiniBoss.h"
+
+EnemyMiniBoss::EnemyMiniBoss(ObjectManager* objManager) :
+	EnemyBase(objManager)
+{
+}
+
+EnemyMiniBoss::~EnemyMiniBoss()
+{
+}
+
+void EnemyMiniBoss::Init()
+{
+}
+
+void EnemyMiniBoss::End()
+{
+}
+
+void EnemyMiniBoss::UpdateEnemy()
+{
+}
+
+void EnemyMiniBoss::Draw()
+{
+}
+
+void EnemyMiniBoss::Attack()
+{
+}
