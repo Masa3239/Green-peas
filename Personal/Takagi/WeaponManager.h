72bf6ba3de12e24a08ec65df0c36d77df464a459
kana-bunbun <gaomuke186@gmail.com>
2026-04-21 14:52:22 +0900

    武器をマネージャー化

---
+++ Personal/Takagi/WeaponManager.h
@@ -0,0 +1,32 @@
+#pragma once
+#include<Vector>
+#include<memory>
+#include"../../Utility/Vector3.h"
+#include"Weapon.h"
+class Weapon;
+class Player;
+class EnemyManager;
+
+class WeaponManager
+{
+public:
+	WeaponManager();
+	~WeaponManager();
+
+	void Init();
+	void End();
+	void Update();
+	void Draw();
+	const void SetPlayer(Player* pPlayer) { m_pPlayer = pPlayer; }
+	void SetObjManager(ObjectManager* objManager) { m_objManager = objManager; }
+	void SetEnemyManager(EnemyManager* enemyManager) { m_enemyManager = enemyManager; }
+	void Create(const Vector3& pos, int weaponType);
+	bool CheckCanPick();
+private:
+	std::vector<std::unique_ptr<Weapon>> m_weapons;
+	Player* m_pPlayer;
+	ObjectManager* m_objManager;
+	EnemyManager* m_enemyManager;
+	float m_createCount;
+};
+
