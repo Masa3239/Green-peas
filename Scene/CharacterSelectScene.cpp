29f5a561e9ad83d0dbc68b31c5494fd902310b3c
kana-bunbun <gaomuke186@gmail.com>
2026-04-27 11:57:46 +0900

    キャラクター選択→ゲームシーンの繊維

--- Scene/CharacterSelectScene.cpp
+++ Scene/CharacterSelectScene.cpp
@@ -5,6 +5,7 @@
 #include"../System/Input/Gamepad.h"
 #include"../Personal/Takagi/RotateCharacter.h"
 #include"../Personal/Takagi/BuffManager.h"
+#include"SceneInGame.h"
 CharacterSelectScene::CharacterSelectScene()
 {
 	GetCarryOver().Reset();
@@ -28,11 +29,12 @@ SceneBase* CharacterSelectScene::Update()
 	m_characters->Update();
 	GetCarryOver().characterJob = m_characters->GetSelectJob();
 	if (InputManager::GetInstance().IsPressed(Input::Action::Confirm)) {
-		return new SceneTestTakagi;
+		return new SceneInGame;
 	}
-	if (Gamepad::GetInstance().IsDown(XINPUT_BUTTON_START)) {
-		return new ResultTestScene;
-	}
+	//if (Gamepad::GetInstance().IsDown(XINPUT_BUTTON_START)) {
+	//	//return new ResultTestScene;
+	//	return new ;
+	//}
 	return this;
 }
 
